957b6b07f1e223968a26d0f396f0106942d8eaae
abhijeetpatil02 <patilabhijeet261@gmail.com>
2026-08-21 19:52:28 +0530

    update

--- backend/model.py
+++ backend/model.py
@@ -9,7 +9,7 @@ model to estimate costs, and serializes both the trained model and label  encode
 to pickle files for subsequent API  production usage..
 Execute this script whenever:
 1. The underlying dataset '../dataset/hospital_data.csv' is updated or replaced.
-2. Model parameters (hyperparameters) or selected algorithms are modifieeed
+2. Model parameters (hyperparameters) or selected algorithms are modifieeed..
 """
 
 import pandas as pd
